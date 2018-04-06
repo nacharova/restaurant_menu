dae51032aab04cb77af771fbd2dd5dca41452e04
snezhana <muginova2014@gmail.com>
2018-04-06 06:45:43 +0500

    Отображение списка блюд. Добавление и отображение картинок.

--- menu/restaurant_menu/forms.py
+++ menu/restaurant_menu/forms.py
@@ -1,9 +1,13 @@
 from django import forms
+from .models import Dish
 
 
-class DishForm(forms.Form):
-    check = forms.CheckboxSelectMultiple()
+class DishForm(forms.ModelForm):
     name = forms.CharField(label='название блюда', max_length=255)
     food_value = forms.IntegerField(label='пищевая ценность')
     price = forms.FloatField(label='цена')
     image = forms.ImageField(label='картинка', required=False)
+
+    class Meta:
+        model = Dish
+        fields = "__all__"
--- menu/restaurant_menu/views.py
+++ menu/restaurant_menu/views.py
@@ -10,31 +10,34 @@ def index(request):
 
 
 def order(request):
+    dishes = Dish.objects.all()
     if request.method == 'POST':
-        form = DishForm(request.POST)
-        if form.is_valid():
-            dishes = Dish.objects.all()
-            checked_dishes = dishes.filter(pk__in=request.POST.get('check', ()))
-            print(checked_dishes)
-            if not checked_dishes:
-                return render(request, 'restaurant_menu/index.html', {
-                    'error_message': "Не выбрано ни одно блюдо.", 'dishes': dishes,
-                })
-            return render(request, 'restaurant_menu/order.html', {'checked_dishes': checked_dishes})
+        checked_dishes = list()
+        total_price = 0
+        for dish in dishes:
+            dish_is_checked = dishes.filter(pk__in=request.POST.get("check_{id}".format(id=dish.id), ()))
+            if dish_is_checked:
+                for item in dish_is_checked:
+                    total_price += item.price
+                checked_dishes.append(dish_is_checked)
+        print(checked_dishes)
+        if not checked_dishes:
+            return render(request, 'restaurant_menu/index.html', {
+                'error_message': "Не выбрано ни одно блюдо.", 'dishes': dishes,
+            })
+        return render(request, 'restaurant_menu/order.html',
+                      {'checked_dishes': checked_dishes, 'total_price': total_price})
+    return render(request, 'restaurant_menu/index.html', {
+        'error_message': "Не выбрано ни одно блюдо.", 'dishes': dishes,
+    })
 
 
 def add_dish(request):
     if request.method == 'POST':
-        form = DishForm(request.POST)
+        form = DishForm(request.POST, request.FILES)
         if form.is_valid():
-            name = request.POST.get('name', ())
-            food_value = request.POST.get('food_value', 0)
-            price = request.POST.get('price', 0)
-            image = request.POST.get('image', None)
-            Dish.objects.create(name=name,
-                                food_value=food_value,
-                                price=price,
-                                image=image)
+            form.save()
+            return render(request, 'restaurant_menu/add_dish.html', {'form_saved': "Блюдо успешно добавлено"})
         else:
             return render(request, 'restaurant_menu/add_dish.html', {'form': form})
     else:
